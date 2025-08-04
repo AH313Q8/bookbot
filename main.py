3fcb058670e71b165f6244d5228546e761a921a5
Kyle on Arch <q80.965@gmail.com>
2025-08-04 14:00:20 +0300

    main.py update stats.py update letter count function added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, letter_count
 
 def get_book_text(book_path):
     with open(book_path) as f: # open file
@@ -8,5 +8,5 @@ def get_book_text(book_path):
 def main():
     file_contents = get_book_text("books/frankenstein.txt")
     print(f"{word_count(file_contents)} words found in the document")
-
+    print(letter_count(file_contents))
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,16 @@
+from marshal import load
 def word_count(file_contents):
     word_list = file_contents.split()
     count = 0
     for words in word_list:
         count += 1
     return count
+
+def letter_count(file_contents):
+    letter_dict = {}
+    for letter in file_contents.lower():
+        if letter in letter_dict:
+            letter_dict[letter] += 1
+        else:
+            letter_dict[letter] = 1
+    return letter_dict
