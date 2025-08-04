b6383258c02c50b993c7547a2874ebc3f5975204
Kyle on Arch <q80.965@gmail.com>
2025-08-04 22:31:07 +0300

    stats sort_on letter_sort functions and main report added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count, letter_count
+from stats import word_count, letter_count, letter_sort
 
 def get_book_text(book_path):
     with open(book_path) as f: # open file
@@ -7,6 +7,18 @@ def get_book_text(book_path):
 
 def main():
     file_contents = get_book_text("books/frankenstein.txt")
-    print(f"{word_count(file_contents)} words found in the document")
-    print(letter_count(file_contents))
+    print(f"=" * 12 + " BOOKBOT " + "=" * 12)
+    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"-" * 11 + " Word Count " + "-" * 11)
+    print(f"Found {word_count(file_contents)} total words")
+    print(f"-" * 9 + " Character Count " + "-" * 9)
+
+    letter_dict = (letter_count(file_contents))
+    sorted_letters = letter_sort(letter_dict)
+
+    for item in sorted_letters:
+        print(f"{item['char']}: {item['num']}")
+
+    print(f"=" * 12 + " END " + "=" * 12)
+
 main()
--- stats.py
+++ stats.py
@@ -1,16 +1,36 @@
-from marshal import load
 def word_count(file_contents):
     word_list = file_contents.split()
     count = 0
+
     for words in word_list:
         count += 1
+
     return count
 
 def letter_count(file_contents):
     letter_dict = {}
+
     for letter in file_contents.lower():
+
         if letter in letter_dict:
             letter_dict[letter] += 1
+
         else:
             letter_dict[letter] = 1
+
     return letter_dict
+
+def sort_on(item):
+    return item["num"]
+
+def letter_sort(letter_dict):
+    letter_list = []
+
+    for letter in letter_dict:
+
+        if letter.isalpha():
+            letter_list.append({"char": letter, "num": letter_dict[letter]})
+
+    letter_list.sort(key=sort_on, reverse=True)
+
+    return letter_list
