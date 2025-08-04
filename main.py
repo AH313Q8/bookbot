eb6678b68acd9abea0dd5ce7bd96466d5149724b
Kyle on Arch <q80.965@gmail.com>
2025-08-05 01:02:03 +0300

    main.py sys.exit() added and final clean up

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count, letter_count, letter_sort
+import sys
 
 def get_book_text(book_path):
     with open(book_path) as f: # open file
@@ -6,12 +7,20 @@ def get_book_text(book_path):
     return file_contents
 
 def main():
-    file_contents = get_book_text("books/frankenstein.txt")
-    print(f"=" * 12 + " BOOKBOT " + "=" * 12)
-    print("Analyzing book found at books/frankenstein.txt...")
-    print(f"-" * 11 + " Word Count " + "-" * 11)
+    book_path = ""
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+
+    file_contents = get_book_text(book_path)
+    print("=" * 12 + " BOOKBOT " + "=" * 12)
+    print(f"Analyzing book found at {book_path}...")
+    print("-" * 11 + " Word Count " + "-" * 11)
     print(f"Found {word_count(file_contents)} total words")
-    print(f"-" * 9 + " Character Count " + "-" * 9)
+    print("-" * 9 + " Character Count " + "-" * 9)
 
     letter_dict = (letter_count(file_contents))
     sorted_letters = letter_sort(letter_dict)
@@ -19,6 +28,6 @@ def main():
     for item in sorted_letters:
         print(f"{item['char']}: {item['num']}")
 
-    print(f"=" * 12 + " END " + "=" * 12)
+    print("=" * 12 + " END " + "=" * 12)
 
 main()
