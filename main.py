587fab07c1ac4c898b871a64fb9f7a4faa9343b8
Kyle on Arch <q80.965@gmail.com>
2025-08-04 12:36:20 +0300

    main.py created read file functions

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(book_path):
+    with open(book_path) as f: # open file
+        file_contents = f.read() # reads file
+    return file_contents
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main()
